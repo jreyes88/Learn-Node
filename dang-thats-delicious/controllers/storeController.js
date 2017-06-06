1be46fa16ab70e26eacad949d02105f4c31c35ab
Joey Reyes <jreyes88@gmail.com>
2017-06-06 10:24:40 -0500

    Completed Unit 3

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,12 +1,13 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Joey';
-  if(req.name === 'Joey') {
-    throw Error('That is a stupid name');
-  }
-  next();
-}
-
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {
+    title: 'Add Store'
+  });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 }
